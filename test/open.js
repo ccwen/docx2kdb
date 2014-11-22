7168288818c8695745787aa92bc8e3fb085bc77b
yap cheah shen <yapcheahshen@gmail.com>
2014-11-23 00:04:33 +0800

    90% ok

--- test/open.js
+++ test/open.js
@@ -1,6 +1,7 @@
 var docx2kdb=require("../lib/index");
 console.time("convert");
 docx2kdb.convertToXML("../sample/憲法.docx",{},function(session){
-	console.log(session.pcount);
+	//console.log(session.pcount);
+	console.log(session.output.join(""));
 	console.timeEnd("convert");	
 });
